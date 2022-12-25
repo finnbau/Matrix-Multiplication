8d3ca18e05ac1d271b19572d7bdc64fdf7c17410
JDawggie <johannes.s1995@gmail.com>
2022-12-25 13:40:42 +0100

    tihi

--- app/src/main/java/matrix_multiplication/AMain.java
+++ app/src/main/java/matrix_multiplication/AMain.java
@@ -24,6 +24,9 @@ public class AMain {
         for(double e:m16Transposed.data){
             System.out.print(e+", ");
         }
+
+        // Totally didn't use the above result to make the test for tranposing n=16 matrices.... ;)
+
         // Matrix A = new Matrix(8, 8, new double[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
         // 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
         // 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
